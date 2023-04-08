4e4bc1bd75bc7aedd7676fac06332e9044fef601
marufbroh <marufmd775@gmail.com>
2023-04-09 02:02:21 +0600

    react-router implemented

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                
+            }
+        ]
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
