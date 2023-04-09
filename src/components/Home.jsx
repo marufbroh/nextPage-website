2863de6bb42cd1bf1a6ad84157492e72a9dfdb9d
marufbroh <marufmd775@gmail.com>
2023-04-09 13:17:51 +0600

    Header section added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <p>Home brodsfadfsghhsffdgfggdfdddddddddddddddddddddddddddddgaaaaaaaaaaaaaaaaaaaaaaaaaaaasfddsfd</p>
+            <p>Home</p>
         </div>
     );
 };
